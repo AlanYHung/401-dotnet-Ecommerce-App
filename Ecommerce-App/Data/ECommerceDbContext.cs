7b916503ab38dcfce3ab10614d421585e21aa798
AlanYHung <72707899+AlanYHung@users.noreply.github.com>
2021-02-15 18:52:09 -0800

    Model Setup/Views Wired up

---
+++ Ecommerce-App/Data/ECommerceDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_App.Data
+{
+  public class ECommerceDbContext
+  {
+{
+    public AsyncInnDbContext(DbContextOptions options) : base(options) { }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Amenity> Amenities { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.Entity<RoomAmenities>().HasKey(
+          roomAmenities => new { roomAmenities.RoomId, roomAmenities.AmenityId });
+      modelBuilder.Entity<HotelRoom>().HasKey(
+         hotelRooms => new { hotelRooms.HotelId, hotelRooms.RoomNumber });
+    }
+
+  } // end of class
+} // end of namespace
