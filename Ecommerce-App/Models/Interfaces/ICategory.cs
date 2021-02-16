2e59240d26017d370d51f29fc45d27ab813c9b09
daviddicken <davidfromseattle@gmail.com>
2021-02-15 20:50:36 -0800

    inteface

--- Ecommerce-App/Models/Interfaces/ICategory.cs
+++ Ecommerce-App/Models/Interfaces/ICategory.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Ecommerce_App.Models.API;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ecommerce_App.Models.Interfaces
 {
-    interface ICategory
+    public interface ICategory
     {
+        Task<Category> Create(CategoryDTO Category);
+        Task<CategoryDTO> GetCategory(int id);
+        Task<List<CategoryDTO>> GetCategories();
+        Task<Category> UpdateCategory(int id, CategoryDTO Category);
+        Task DeleteCategory(int id);
+        Task AddProduct(int CategoryId, int ProductId);
+        Task RemoveProduct(int CategoryId, int ProductId);
+
     }
 }
